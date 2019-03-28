a1eaffd7f777812021868f4f62256e3b06ae82fa
metroid112 <federico.aguilera112@gmail.com>
2019-03-28 17:17:40 -0300

    File processing

--- src/bot.py
+++ src/bot.py
@@ -1,28 +1,66 @@
-import facebook
+import fnmatch
+import os
 import pprint
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-ANHS_ACCESS_TOKEN = 'EAACVS6jUj0QBABAyhEcutQYUVZAnjq55BAYrAt5VzAzabAhn5x8KOmxZBrWB9cpNcIvQanOpMMfQCN3ZBLZACAlZC7ccs7hcwVTMTmbkTsdvhomhSIShzFn19DUajPV2syfYOQwjOrmN13nq7wLguoPTgbOmIClsh1wRU8LAAeEuNbbdYHX5b'
-ANHS_ID = 1456333264610651
+import facebook
 
+# PATO_ID = 365219843638526
+# ANHS_ID = 1456333264610651
 PATO_ACCESS_TOKEN = 'EAACVS6jUj0QBAG2n9qdrga7ZBjytXpaCdkDfNPscmtQDzmDWoMFqnHFApvQtrYjv2ycQhno7u7Fp8fB7EZC2tzlHj3rIIRTIZAKlFZA71DaJZAQyecMdNul0QkMR8Ryy6fhclYAaxUXWsqiAKHZAqu43Lk3JFYltQJLPHnJLOZB57ZCzDluoRD8YBO42VInFGckZD'
-PATO_ID = 365219843638526
+ANHS_ACCESS_TOKEN = 'EAACVS6jUj0QBABAyhEcutQYUVZAnjq55BAYrAt5VzAzabAhn5x8KOmxZBrWB9cpNcIvQanOpMMfQCN3ZBLZACAlZC7ccs7hcwVTMTmbkTsdvhomhSIShzFn19DUajPV2syfYOQwjOrmN13nq7wLguoPTgbOmIClsh1wRU8LAAeEuNbbdYHX5b'
+
+sched = BlockingScheduler()
 
 
 def main():
-    token = PATO_ACCESS_TOKEN
+    token = ANHS_ACCESS_TOKEN
+
+    files = os.listdir()
+    image_files = []
+    for file in files:
+        if (os.path.isfile(file)) and is_image(file):
+            image_files.append(file)
+
+    pprint.pprint(image_files)
+    pprint.pprint(files)
 
     graph = facebook.GraphAPI(
         access_token=token,
         version="3.1"
     )
 
+    pprint.pprint(graph)
+
+    post_caption = 'Perhaps\nBot test by Metroid'
     post_id = graph.put_photo(
-        image=open('img.jpg', 'rb'),
-        message='Look at this cool photo!'
+        image=open('asdimg.png', 'rb'),
+        message=post_caption
     )
 
     pprint.pprint(post_id)
 
 
-if __name__ == '__main__':
-    main()
+def is_image(file):
+    return (
+        fnmatch.fnmatch(file, '*.png')
+        or fnmatch.fnmatch(file, '*.jpeg')
+        or fnmatch.fnmatch(file, '*.jpg')
+    )
+
+
+@sched.scheduled_job('interval', seconds=5)
+def bot_job():
+    print('This job is run every 5 seconds.')
+    file = os.listdir()[0]
+    print(file)
+    if not(os.path.isdir(file)):
+        if file == 'bot.py':
+            file = os.listdir()[1]
+            print(file)
+        os.remove(file)
+
+    # main()
+
+
+sched.start()
