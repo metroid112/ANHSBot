e32aac3b61bac320c2804765e0dab6059f8d9293
metroid112 <federico.aguilera112@gmail.com>
2019-03-29 16:53:44 -0300

    tomo bot

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,10 @@
 import fnmatch
 import os
-import pprint
+import re
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+import urllib
+import io
 
 import facebook
 
@@ -9,36 +12,80 @@ import facebook
 # ANHS_ID = 1456333264610651
 PATO_ACCESS_TOKEN = 'EAACVS6jUj0QBAG2n9qdrga7ZBjytXpaCdkDfNPscmtQDzmDWoMFqnHFApvQtrYjv2ycQhno7u7Fp8fB7EZC2tzlHj3rIIRTIZAKlFZA71DaJZAQyecMdNul0QkMR8Ryy6fhclYAaxUXWsqiAKHZAqu43Lk3JFYltQJLPHnJLOZB57ZCzDluoRD8YBO42VInFGckZD'
 ANHS_ACCESS_TOKEN = 'EAACVS6jUj0QBABAyhEcutQYUVZAnjq55BAYrAt5VzAzabAhn5x8KOmxZBrWB9cpNcIvQanOpMMfQCN3ZBLZACAlZC7ccs7hcwVTMTmbkTsdvhomhSIShzFn19DUajPV2syfYOQwjOrmN13nq7wLguoPTgbOmIClsh1wRU8LAAeEuNbbdYHX5b'
+latest_tomo_chapter = 911 # read latest chapter
 
 sched = BlockingScheduler()
 
 
-def main():
-    token = ANHS_ACCESS_TOKEN
+@sched.scheduled_job('interval', seconds=5)
+def bot_job():
+    print('------------------------')
+    print('Running every 10 seconds')
 
-    files = os.listdir()
-    image_files = []
-    for file in files:
-        if (os.path.isfile(file)) and is_image(file):
-            image_files.append(file)
-
-    pprint.pprint(image_files)
-    pprint.pprint(files)
+    token = PATO_ACCESS_TOKEN
+    print('Connected with token:', token)
 
     graph = facebook.GraphAPI(
         access_token=token,
         version="3.1"
     )
 
-    pprint.pprint(graph)
+    files = os.listdir()
+    print('Files:', files)
 
-    post_caption = 'Perhaps\nBot test by Metroid'
-    post_id = graph.put_photo(
-        image=open('asdimg.png', 'rb'),
-        message=post_caption
-    )
+    image_files = []
+    for file in files:
+        if (os.path.isfile(file)) and is_image(file):
+            print('Appending file:', file)
+            image_files.append(file)
 
-    pprint.pprint(post_id)
+    if len(image_files) > 0:
+        image_file = image_files[0]
+        print('Image files:', image_files)
+        print(image_file)
+        manga_name = re.search('(.+?) - ', image_file).group(1)
+        print('Name before fix:', manga_name)
+        manga_name = manga_name.replace('_', ' ')
+        print('Final name:', manga_name)
+
+        post_caption = 'Manga: ' + manga_name + '\n\n-Bottroid'
+        post_id = graph.put_photo(
+            image=open(image_file, 'rb'),
+            message=post_caption
+        )
+
+        print('Post id:', post_id)
+        if post_id != '':
+            print('Moving file to processed:', image_file)
+            if not os.path.isdir('processed'):
+                os.mkdir('processed')
+            destination = 'processed/' + image_file
+            os.rename(image_file, destination)
+    else:
+        print('No files to process, It\'s Tomo time')
+        now = datetime.datetime.now()
+        print('Time:', now)
+        tomo_chapter = str(latest_tomo_chapter + 1)
+        tomo_url = 'https://dropoutmanga.files.wordpress.com/' + str(now.year) + '/' + str(now.month).zfill(2) + '/dropout-tomo-chan-wa-onna-no-ko-page-' + tomo_chapter + '.png'
+        print('URL:', tomo_url)
+        try:
+            tomo_url_file = urllib.request.urlopen(tomo_url)
+            tomo_file = io.BytesIO(tomo_url_file.read())
+
+            post_caption = 'Manga: Tomo-chan wa Onnanoko ' + tomo_chapter + '\n\n-Bottroid'
+            try:
+                post_id = graph.put_photo(
+                    image=tomo_file,
+                    message=post_caption
+                )
+
+                print('Post id:', post_id)
+
+                # update tomo chapter
+            except facebook.GraphAPIError:
+                print('Couldn\'t post chapter')
+        except urllib.error.HTTPError:
+            print('No chapter found')
 
 
 def is_image(file):
@@ -46,21 +93,8 @@ def is_image(file):
         fnmatch.fnmatch(file, '*.png')
         or fnmatch.fnmatch(file, '*.jpeg')
         or fnmatch.fnmatch(file, '*.jpg')
+        or fnmatch.fnmatch(file, '*.webp')
     )
-
-
-@sched.scheduled_job('interval', seconds=5)
-def bot_job():
-    print('This job is run every 5 seconds.')
-    file = os.listdir()[0]
-    print(file)
-    if not(os.path.isdir(file)):
-        if file == 'bot.py':
-            file = os.listdir()[1]
-            print(file)
-        os.remove(file)
-
-    # main()
 
 
 sched.start()
